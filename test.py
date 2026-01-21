cce45ecb16f789ed95fe2937a03ebc3e01086380
Yves Tanas <Yves.Tanas@gmx.de>
2026-01-21 11:05:36 +0100

    s

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def test_cities():
     assert wiki.check_answer("Fulda", "stadt", "f") == True, "Diese Stadt existiert nicht"
     assert wiki.if_exists_in_wiki("Franz") == True, "Diese Seite existiert nicht bei Wikipedia"
     assert wiki.check_answer("Frankfurt (Oder)", "stadt", "f") == True, "Diese Stadt existiert nicht"
-    assert wiki.check_answer("Fulda", "fluss", "f") == True, "Dieser Fluss existiert nicht"
+    assert wiki.check_answer("Hamburg", "stadt", "h") == True, "Dieser Fluss existiert nicht"
 
 
 if __name__ == "__main__":
