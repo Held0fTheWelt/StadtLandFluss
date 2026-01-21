219ce8c5c779588191fe682cd348b7e76f1f230f
Yves Tanas <Yves.Tanas@gmx.de>
2026-01-21 14:15:13 +0100

    save

--- color.py
+++ color.py
@@ -1,5 +1,5 @@
 """
-ANSI color codes
+ANSI Farb codes
 """
 RED = "\033[0;31m"
 GREEN = "\033[0;32m"
--- test.py
+++ test.py
@@ -3,25 +3,35 @@ import wiki
 import backend
 
 def test_Berlin():
-    """ Berlin als Eingabe für Stadt"""
+    """
+    Berlin als Eingabe für Stadt
+    """
     assert wiki.check_answer("Berlin", "stadt", "b") == True, "Diese Stadt existiert nicht"
 
 def test_Fulda():
-    """ Fulda als Eingabe für Stadt"""
+    """
+    Fulda als Eingabe für Stadt
+    """
     assert wiki.check_answer("Fulda", "stadt", "f") == True, "Diese Stadt existiert nicht"
     assert wiki.check_answer("Fulda", "fluss", "f") == True, "Dieser Fluss existiert nicht"
 
 def test_Frankfurt():
-    """ Frankfurt als Eingabe für Stadt"""
+    """
+    Frankfurt als Eingabe für Stadt
+    """
     assert wiki.if_exists_in_wiki("Frankfurt") == True, "Diese Seite existiert nicht bei Wikipedia"
     assert wiki.check_answer("Frankfurt", "stadt", "f") == True, "Diese Stadt existiert nicht"
 
 def test_Hamburg():
-    """ Hamburg als Eingabe für Stadt"""
+    """
+    Hamburg als Eingabe für Stadt
+    """
     assert wiki.check_answer("Hamburg", "stadt", "h") == True, "Diese Stadt existiert nicht"
 
 def test_Stadt_Richtig_Aber_Buchstabe_Falsch():
-    """ Prüft, ob Buchstabe zur Eingabe als Anfangsbuchstabe passt"""
+    """
+    Prüft, ob Buchstabe zur Eingabe als Anfangsbuchstabe passt
+    """
     assert wiki.check_answer("Hamburg", "stadt", "g") == False, "Der Buchstabe wird nicht richtig geprüft"
 
 
